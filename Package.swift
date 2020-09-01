3f660f740eb9a7239f7a4cd44b19be9bf208b481
Valentin Kuznetsov <vkuznet@gmail.com>
2020-09-01 08:55:02 -0400

    Add example for using local dir

--- Package.swift
+++ Package.swift
@@ -9,6 +9,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vkuznet/MyFirstSwiftLibrary", from: "1.0.0"),
+        // if you need to refer to local area please use the following
+        // you may use absolute or relative path, and HASH_STRING
+        // of git commit you need to refer
+        // .package(url: "../MyFirstSwiftLibrary", .revision("HASH_STRING")),
     ],
     targets: [
         .target(name: "MyFirstSwiftApp", dependencies: ["MyFirstSwiftLibrary"]),
